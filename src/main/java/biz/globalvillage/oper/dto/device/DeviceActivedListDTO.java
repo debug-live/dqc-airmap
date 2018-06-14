1c4ccf392b5b8a9229525ccd823aa72ce70440ce
soleray <pein_zero@126.com>
2018-06-14 15:54:15 +0800

    device param and return

--- src/main/java/biz/globalvillage/oper/dto/device/DeviceActivedListDTO.java
+++ src/main/java/biz/globalvillage/oper/dto/device/DeviceActivedListDTO.java
@@ -1,4 +1,47 @@
 package biz.globalvillage.oper.dto.device;
 
 public class DeviceActivedListDTO {
+
+    private String deviceSn;
+
+    private String nickName;
+
+    private String connectStateStr;
+
+    private String companyName;
+
+    //辅助字段
+    private int isConnect;
+
+    public String getDeviceSn() {
+        return deviceSn;
+    }
+
+    public void setDeviceSn(String deviceSn) {
+        this.deviceSn = deviceSn;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getConnectStateStr() {
+        return connectStateStr;
+    }
+
+    public void setConnectStateStr(String connectStateStr) {
+        this.connectStateStr = connectStateStr;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
 }
--- src/main/java/biz/globalvillage/oper/param/device/DeviceActivedListParam.java
+++ src/main/java/biz/globalvillage/oper/param/device/DeviceActivedListParam.java
@@ -1,4 +1,26 @@
 package biz.globalvillage.oper.param.device;
 
-public class DeviceActivedListParam {
+import biz.globalvillage.airmap.param.page.PageParam;
+
+public class DeviceActivedListParam extends PageParam {
+
+    private String deviceSn;
+
+    private long categoryId;
+
+    public String getDeviceSn() {
+        return deviceSn;
+    }
+
+    public void setDeviceSn(String deviceSn) {
+        this.deviceSn = deviceSn;
+    }
+
+    public long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(long categoryId) {
+        this.categoryId = categoryId;
+    }
 }
