a2318a63ad1494070de8d14f78b482d2b4d8e7ae
soleray <pein_zero@126.com>
2018-06-15 17:58:54 +0800

    net param and return

--- src/main/java/biz/globalvillage/oper/controller/net/NetBrokerController.java
+++ src/main/java/biz/globalvillage/oper/controller/net/NetBrokerController.java
@@ -2,46 +2,57 @@ package biz.globalvillage.oper.controller.net;
 
 import biz.globalvillage.airmap.bean.res.Result;
 import biz.globalvillage.airmap.utils.resp.ResponseUtil;
+import biz.globalvillage.common.entity.net.NetBroker;
+import biz.globalvillage.oper.dto.net.NetBrokerListDTO;
+import biz.globalvillage.oper.dto.net.NetListDTO;
+import biz.globalvillage.oper.param.net.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/oper/net/broker")
 public class NetBrokerController {
 
     @PostMapping("/save")
-    public Result saveNetBroker(){
-        return ResponseUtil.setDefaultSuccessResponse();
+    public Result saveNetBroker(NetBroker netBroker){
+        return ResponseUtil.setDefaultErrResponse();
     }
 
     @PostMapping("/list")
-    public Result getNetBrokerList(){
-        return ResponseUtil.setDefaultSuccessResponse();
+    public Result<List<NetBrokerListDTO>> getNetBrokerList(NetBrokerListParam param){
+        List<NetBrokerListDTO> list = new ArrayList<>();
+        return ResponseUtil.setSuccessDataResponse(list);
     }
 
     @PostMapping("/detail")
-    public Result getNetBrokerDetail(){
-        return ResponseUtil.setDefaultSuccessResponse();
+    public Result getNetBrokerDetail(NetBroker param){
+        NetBroker netBroker = new NetBroker();
+        return ResponseUtil.setSuccessDataResponse(netBroker);
     }
 
     @PostMapping("/bindNet")
-    public Result bindNet(){
+    public Result bindNet(NetBrokerRelAddParam param){
         return ResponseUtil.setDefaultSuccessResponse();
     }
 
     @PostMapping("/unbindNet")
-    public Result unbindNet(){
+    public Result unbindNet(NetBrokerRelDelParam param){
         return ResponseUtil.setDefaultSuccessResponse();
     }
 
     @PostMapping("/bindedNet/list")
-    public Result getBindedNetList(){
-        return ResponseUtil.setDefaultSuccessResponse();
+    public Result<List<NetListDTO>> getBindedNetList(NetBindedListParam param){
+        List<NetListDTO> list = new ArrayList<>();
+        return ResponseUtil.setSuccessDataResponse(list);
     }
 
     @PostMapping("/unbindedNet/list")
-    public Result getUnBindedNetList(){
-        return ResponseUtil.setDefaultSuccessResponse();
+    public Result<List<NetListDTO>> getUnBindedNetList(NetUnbindListParam param){
+        List<NetListDTO> list = new ArrayList<>();
+        return ResponseUtil.setSuccessDataResponse(list);
     }
 }
